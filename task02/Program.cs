9a3eaca005ebf0b64020f806a050c35b6358c800
andrey <13rred@gmail.com>
2023-07-23 22:16:05 +0300

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Promt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int Sum(int a)
+{
+    int count = 0;
+    for (; a > 0; a/=10)
+    
+    {
+        count =count + a%10;
+    }
+    return count;
+
+}
+
+int num = Promt("Введите число: ");
+Console.WriteLine($"Сумма цифр в числе {Sum(num)}");
